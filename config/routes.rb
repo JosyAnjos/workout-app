4d33056bc836443db7ce134152e90184351b8475
Josy <josyanjos.souza@gmail.com>
2019-08-06 11:42:23 -0300

    wip: Add routes, index and new actions of exercises controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root to: "dashboards#index"
-resources :dashboards, only: [:index] do
+  root to: "dashboards#index"
+  resources :dashboards, only: [:index] do
     collection do
       post :search, to: 'dashboards#search'
     end
   end
+  resources :users do 
+    resources :exercises
+  end  
 end
